709dcde473d3cf77a8a8ee347d0c9c11bec964e7
tanvir raj <rajbp02@gamil.com>
2017-08-01 08:49:09 +0600

    Single Product Detail View

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', index, name="index"),
+    url(r'^(?P<product_id>[0-9]+)$', detail, name="detail"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Product
+
 
 
 def index(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'inventory/index.html', context)
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'inventory/detail.html', {'product': product})
